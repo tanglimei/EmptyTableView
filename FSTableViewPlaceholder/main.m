9ee4b50ed984096bf592dea69f3963d69558dd67
巩柯 <GIKI@gikidembp.lan>
2016-05-16 00:42:44 +0800

    Initial Commit

---
+++ FSTableViewPlaceholder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FSTableViewPlaceholder
+//
+//  Created by 巩柯 on 16/5/16.
+//  Copyright © 2016年 GIKI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
